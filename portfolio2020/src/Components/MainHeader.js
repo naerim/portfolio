47c6e10df54c971ae3bf404650ca233de408a19a
naerim <naerim1119@gmail.com>
2020-06-03 03:14:31 +0900

    main Header

---
+++ portfolio2020/src/Components/MainHeader.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const MainHeader = () => {
+  return (
+    <div className="Menu">
+      <div className="Logo">
+        <img src="#" alt="LogoImage" />
+      </div>
+      <ul>
+        <li>ABOUT</li>
+        <li>PROJECTS</li>
+        <li>CONTACT</li>
+        <li>LOGIN</li>
+      </ul>
+    </div>
+  );
+};
+
+export default MainHeader;
