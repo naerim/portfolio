32470944f7322fddadb1090df8add4a510191b25
naerim <naerim1119@gmail.com>
2020-06-03 03:14:50 +0900

    main Contents

---
+++ portfolio2020/src/Components/MainSection.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MainSection = () => {
+    return (
+        <section className="Contents">
+            <div className="Introduce">
+                <h1>Hi! I'm Developer Kim Nae-Rim</h1>
+                <h4>This is my portfolio website</h4>
+            </div>
+        </section>
+    );
+};
+
+export default MainSection;
