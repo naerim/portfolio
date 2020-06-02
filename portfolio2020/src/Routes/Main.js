0051559e879ee14a2aa5b4af4440db3516f16a81
naerim <naerim1119@gmail.com>
2020-06-03 03:15:13 +0900

    main page

---
+++ portfolio2020/src/Routes/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import MainHeader from "../Components/MainHeader";
+import MainSection from "../Components/MainSection";
+import MainFooter from "../Components/MainFooter";
+
+const Main = () => {
+  return (
+      <div>
+          <MainHeader/>
+          <MainSection/>
+          <MainFooter/>
+      </div>
+  )
+};
+
+export default Main;
