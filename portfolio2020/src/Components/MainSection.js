b9646db2ddcf5813649921c95ee4ac75c2e69295
naerim <naerim1119@gmail.com>
2020-06-05 02:51:25 +0900

    portfolio 배경이미지 추가

--- portfolio2020/src/Components/MainSection.js
+++ portfolio2020/src/Components/MainSection.js
@@ -1,14 +1,21 @@
 import React from "react";
+import styled from "styled-components";
+
+const Introduce = styled.div``;
+
+const Title = styled.h1`
+  color: white;
+`;
 
 const MainSection = () => {
-    return (
-        <section className="Contents">
-            <div className="Introduce">
-                <h1>Hi! I'm Developer Kim Nae-Rim</h1>
-                <h4>This is my portfolio website</h4>
-            </div>
-        </section>
-    );
+  return (
+    <section className="Contents">
+      <Introduce>
+        <Title>Hi! I'm Developer Kim Nae-Rim</Title>
+        <h4>This is my portfolio website</h4>
+      </Introduce>
+    </section>
+  );
 };
 
 export default MainSection;
--- portfolio2020/src/Main.js
+++ portfolio2020/src/Main.js
@@ -1,13 +1,22 @@
 import React from "react";
 import MainHeader from "./Components/MainHeader";
 import MainSection from "./Components/MainSection";
+import styled from "styled-components";
+
+const Background = styled.div`
+  background-image: url("https://github.com/naerim/portfolio/blob/master/portfolio2020/src/img/mainBackground.png?raw=true");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+  height: 100vh;
+`;
 
 const Main = () => {
   return (
-    <div>
+    <Background>
       <MainHeader />
       <MainSection />
-    </div>
+    </Background>
   );
 };
 
