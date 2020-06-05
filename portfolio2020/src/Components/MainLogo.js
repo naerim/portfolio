88ca0f31968be63efb0dcc4f092515e23283e5b0
naerim <naerim1119@gmail.com>
2020-06-05 23:01:42 +0900

    logo

---
+++ portfolio2020/src/Components/MainLogo.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+import Logo from "../img/logo.png";
+
+const ImgBox = styled.div`
+  width: 110px;
+`;
+const Img = styled.img`
+  width: 100%;
+  float: center;
+`;
+
+const MainLogo = () => {
+  return (
+    <ImgBox>
+      <Img src={Logo} alt="Logo" />
+    </ImgBox>
+  );
+};
+
+export default MainLogo;
