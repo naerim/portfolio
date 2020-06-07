f974a786131847c3d4081fe93001f44f8bb58e7e
naerim <naerim1119@gmail.com>
2020-06-07 17:36:38 +0900

    logo 크기 수정

--- portfolio2020/src/Components/MainLogo.js
+++ portfolio2020/src/Components/MainLogo.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import Logo from "../img/logo.png";
 
 const ImgBox = styled.div`
-  width: 110px;
+  width: 40px;
 `;
 const Img = styled.img`
   width: 100%;
