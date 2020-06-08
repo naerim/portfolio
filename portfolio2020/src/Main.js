4234b403cc23a8e395ed1c41d9137c4a7b99e8f7
naerim <naerim1119@gmail.com>
2020-06-08 18:31:48 +0900

    remove background-image

--- portfolio2020/src/Main.js
+++ portfolio2020/src/Main.js
@@ -3,32 +3,24 @@ import MainHeader from "./Components/MainHeader";
 import MainSection from "./Components/MainSection";
 import styled from "styled-components";
 
-const Game = styled.div`
-  margin: 0;
-  padding: 30px;
+// background-image: url("https://github.com/naerim/portfolio/blob/master/portfolio2020/src/img/mainBackground.png?raw=true");
+// background-size: cover;
+// background-repeat: no-repeat;
+// background-position: center center;
 
-  background: rgb(80,80,80);
-  height: 100vh;
-`;
 const Background = styled.div`
-  background-image: url("https://github.com/naerim/portfolio/blob/master/portfolio2020/src/img/mainBackground.png?raw=true");
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center center;
+  background-color: #000033;
   width: 100%;
-  height: 90vh;
-  
-    border-radius: 10px
+  height: 100vh;
+  border-radius: 10px;
 `;
 
 const Main = () => {
   return (
-    <Game>
-      <Background>
-        <MainHeader />
-        <MainSection />
-      </Background>
-    </Game>
+    <Background>
+      <MainHeader />
+      <MainSection />
+    </Background>
   );
 };
 
