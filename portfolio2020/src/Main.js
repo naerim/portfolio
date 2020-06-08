5f7e914c5a2b373ed0bbbd3c1f0fe7f7a7b7e190
naerim <naerim1119@gmail.com>
2020-06-08 16:11:09 +0900

    이미지 테두리

--- portfolio2020/src/Main.js
+++ portfolio2020/src/Main.js
@@ -3,21 +3,32 @@ import MainHeader from "./Components/MainHeader";
 import MainSection from "./Components/MainSection";
 import styled from "styled-components";
 
+const Game = styled.div`
+  margin: 0;
+  padding: 30px;
+
+  background: rgb(80,80,80);
+  height: 100vh;
+`;
 const Background = styled.div`
   background-image: url("https://github.com/naerim/portfolio/blob/master/portfolio2020/src/img/mainBackground.png?raw=true");
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center center;
   width: 100%;
-  height: 750px;
+  height: 90vh;
+  
+    border-radius: 10px
 `;
 
 const Main = () => {
   return (
-    <Background>
-      <MainHeader />
-      <MainSection />
-    </Background>
+    <Game>
+      <Background>
+        <MainHeader />
+        <MainSection />
+      </Background>
+    </Game>
   );
 };
 
