ee120e56321d389efdb93e37680887680e14ddff
naerim <naerim1119@gmail.com>
2020-06-10 00:31:32 +0900

    배경화면 background-attachment: fixed

--- portfolio2020/src/App.js
+++ portfolio2020/src/App.js
@@ -1,16 +1,28 @@
 import React from "react";
 import Main from "./Main";
 import About from "./About";
-import MainFooter from "./Components/MainFooter";
+import MainFooter from "./Components/Main/MainFooter";
 import Projects from "./Projects";
+import styled from "styled-components";
+
+const Background = styled.div`
+  background-image: url("https://github.com/naerim/portfolio/blob/master/portfolio2020/src/img/cloud.png?raw=true");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+  margin: 0;
+  background-attachment: fixed;
+`;
 
 function App() {
   return (
     <div className="App">
-      <Main />
-      <About/>
-      <Projects/>
-      <MainFooter/>
+      <Background>
+        <Main />
+        <About />
+        <Projects />
+        <MainFooter />
+      </Background>
     </div>
   );
 }
