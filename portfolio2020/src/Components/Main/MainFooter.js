9b4c725708ce2b16269aebf46812ade59a76bbe2
naerim <naerim1119@gmail.com>
2020-06-12 03:03:14 +0900

    Footer 추가

--- portfolio2020/src/Components/Main/MainFooter.js
+++ portfolio2020/src/Components/Main/MainFooter.js
@@ -1,19 +1,56 @@
 import React from "react";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBloggerB,
+  faFacebook,
+  faGithub,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+
+const Footer = styled.div`
+  width: 100%;
+  height: 100px;
+`;
+
+const Icon = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Bottom = styled.h6`
+  display: flex;
+  justify-content: center;
+  color: #2D2300;
+  margin-top: 10px;
+  margin-bottom: 0;
+`;
+
+const IconTag = styled.a`
+  margin: 15px 15px;
+  text-decoration: none;
+`;
 
 const MainFooter = () => {
   return (
-    <div className="Footer">
-      <div className="Icon">
-        <img src="#" alt="github" />
-        <img src="#" alt="facebook" />
-        <img src="#" alt="tistory" />
-        <img src="#" alt="youtube" />
-      </div>
-      <span className="Email">
-        <h5>E-mail: naerim1119@naver.com</h5>
-      </span>
-      <h6>©2020 by portfolio Proudly created with Kim Nae-Rim </h6>
-    </div>
+    <Footer>
+      <Icon>
+        <IconTag href="/">
+          <FontAwesomeIcon icon={faGithub} color="#2D2300" size="2x" />
+        </IconTag>
+        <IconTag href="/">
+          <FontAwesomeIcon icon={faFacebook} color="#2D2300" size="2x" />
+        </IconTag>
+        <IconTag href="/">
+          <FontAwesomeIcon icon={faBloggerB} color="#2D2300" size="2x" />
+        </IconTag>
+        <IconTag href="/">
+          <FontAwesomeIcon icon={faYoutube} color="#2D2300" size="2x" />
+        </IconTag>
+      </Icon>
+      <Bottom>©2020 by portfolio Proudly created with Kim Nae-Rim </Bottom>
+    </Footer>
   );
 };
 
