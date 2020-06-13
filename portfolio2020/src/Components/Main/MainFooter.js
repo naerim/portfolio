e508853a5e5cc0f3266075a0bdba0dc5e0049a48
naerim <naerim1119@gmail.com>
2020-06-13 23:01:00 +0900

    Fix MainFooter

--- portfolio2020/src/Components/Main/MainFooter.js
+++ portfolio2020/src/Components/Main/MainFooter.js
@@ -1,12 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faBloggerB,
-  faFacebook,
-  faGithub,
-  faYoutube,
-} from "@fortawesome/free-brands-svg-icons";
+import IconData from "../../Data/iconData";
 
 const Footer = styled.div`
   width: 100%;
@@ -22,32 +16,16 @@ const Icon = styled.div`
 const Bottom = styled.h6`
   display: flex;
   justify-content: center;
-  color: #2D2300;
+  color: #2d2300;
   margin-top: 10px;
   margin-bottom: 0;
-`;
-
-const IconTag = styled.a`
-  margin: 15px 15px;
-  text-decoration: none;
 `;
 
 const MainFooter = () => {
   return (
     <Footer>
       <Icon>
-        <IconTag href="/">
-          <FontAwesomeIcon icon={faGithub} color="#2D2300" size="2x" />
-        </IconTag>
-        <IconTag href="/">
-          <FontAwesomeIcon icon={faFacebook} color="#2D2300" size="2x" />
-        </IconTag>
-        <IconTag href="/">
-          <FontAwesomeIcon icon={faBloggerB} color="#2D2300" size="2x" />
-        </IconTag>
-        <IconTag href="/">
-          <FontAwesomeIcon icon={faYoutube} color="#2D2300" size="2x" />
-        </IconTag>
+        <IconData />
       </Icon>
       <Bottom>©2020 by portfolio Proudly created with Kim Nae-Rim </Bottom>
     </Footer>
