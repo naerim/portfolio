ba0d4149ed9015748a579fc6effcaf5c77de5498
naerim <naerim1119@gmail.com>
2020-06-21 02:54:25 +0900

    Edit Skills

--- portfolio2020/src/Components/About/Skills.js
+++ portfolio2020/src/Components/About/Skills.js
@@ -1,10 +1,11 @@
 import React from "react";
 import styled from "styled-components";
+import SkillData from "../../Data/skillData";
 
 const SkillsSection = styled.section`
   height: 100px;
   margin: 10px 50px;
-    display: inline-block;
+  display: inline-block;
 `;
 
 const SkillDiv = styled.div`
@@ -16,30 +17,7 @@ const Skills = () => {
   return (
     <SkillsSection>
       <SkillDiv>
-        <div className="CPlusPlus">
-          <h3>C++</h3>
-          <div> </div>
-        </div>
-        <div className="Java">
-          <h3>JAVA</h3>
-          <div> </div>
-        </div>
-        <div className="HTML">
-          <h3>HTML</h3>
-          <div> </div>
-        </div>
-        <div className="CSS">
-          <h3>CSS</h3>
-          <div> </div>
-        </div>
-        <div className="Javascript">
-          <h3>Javascript</h3>
-          <div> </div>
-        </div>
-        <div className="React">
-          <h3>React</h3>
-          <div> </div>
-        </div>
+        <SkillData />
       </SkillDiv>
     </SkillsSection>
   );
